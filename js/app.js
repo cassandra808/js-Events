236c8029d33dd3a7941ed4ee116da6b04dff772e
Cassandra Loh <cassandra@Cassandras-MacBook-Air.local>
2018-04-23 18:56:26 -1000

    completed random:

--- js/app.js
+++ js/app.js
@@ -78,7 +78,13 @@ function mrButton (){
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+var randomQuote = document.getElementById ("random")
+randomQuote.addEventListener ("click", randomThing)
 
+function randomThing (){
+    var qElem = document.getElementById("displayQuotes");
+    qElem.innerHTML = quotes;
+}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
